aaeb7210a149172d560ec9eff8c9121f1b5a95e7
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-22 01:10:28 -0700

    renaming works and added script, next-> separate extension when renaming

--- bulk-rename.js
+++ bulk-rename.js
@@ -9,35 +9,34 @@ const { program } = require("commander");
 program
   .version("1.0.0")
   .description("Bulk rename files in directory")
-  .requiredOption("-p, --path <directory>", "Input directory containing files")
-  .option(
-    "-r, --replace <search> <replace>",
-    "Search and replace text in filenames"
-  )
+  .requiredOption("-d, --dir <directory>", "Input directory containing files")
+  .option("-r, --replace [strings...]", "Search and replace text in filenames")
   .parse(process.argv);
 
 // Parse command line arguments
 const {
-  directory: path,
+  dir: dir,
   replace: [search, replace],
 } = program.opts();
 
+console.log(search, replace);
+
 // Check if input exists
-if (!fs.existsSync(directory)) {
+if (!fs.existsSync(dir)) {
   console.error("Input directory does not exist");
   process.exit(1);
 }
 
 // Rename File
 function renameFile(filePath, search, replace) {
-  const dir = path.dirname(filePath); // extracts directory portion of file path, excluding filename itself
+  const directory = path.dirname(filePath); // extracts directory portion of file path, excluding filename itself
   const fileName = path.basename(filePath); // extracts base name (last portion) of a file path
 
   const newFileName = fileName.replace(search, replace); // looks for substring "search" and replaces it with "replace"
-  const newFilePath = path.join(dir, newFileName); // create new file path for newly named file
+  const newFilePath = path.join(directory, newFileName); // create new file path for newly named file
 
   try {
-    fs.rename.Sync(filePath, newFilePath); // renames file using old file path and new file path
+    fs.renameSync(filePath, newFilePath); // renames file using old file path and new file path
     console.log(`Renamed ${fileName} to ${newFileName}`);
   } catch (error) {
     console.error(`Error renaming ${fileName}: ${error.message}`);
@@ -45,13 +44,13 @@ function renameFile(filePath, search, replace) {
 }
 
 // Bulk rename files in directory
-function renameFiles(dir, search, replace) {
-  const files = fs.readdirSync(dir); // scans directory's files
+function renameFiles(directory, search, replace) {
+  const files = fs.readdirSync(directory); // scans directory's files
 
   for (const file of files) {
-    const filePath = path.join(dir, file); // create file path
+    const filePath = path.join(directory, file); // create file path
     renameFile(filePath, search, replace);
   }
 }
 
-renameFiles(directory, search, replace);
+renameFiles(dir, search, replace);
